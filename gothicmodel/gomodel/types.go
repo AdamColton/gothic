f32fbbacc9f0f0341afc0777ebec0cb80054c8cc
Adam Colton <adamcolton@gmail.com>
2018-05-28 16:57:23 -0400

    Changed Types to use Const strings

--- gothicmodel/gomodel/types.go
+++ gothicmodel/gomodel/types.go
@@ -4,29 +4,52 @@ import (
 	"github.com/adamcolton/gothic/gothicgo"
 )
 
+const (
+	Bool       = "bool"
+	Byte       = "byte"
+	Int        = "int"
+	Int8       = "int8"
+	Int16      = "int16"
+	Int32      = "int32"
+	Int64      = "int64"
+	Complex128 = "complex128"
+	Complex64  = "complex64"
+	Float32    = "float32"
+	Float64    = "float64"
+	Rune       = "rune"
+	String     = "string"
+	Uint       = "uint"
+	Uint8      = "uint8"
+	Uint16     = "uint16"
+	Uint32     = "uint32"
+	Uint64     = "uint64"
+	Uintptr    = "uintptr"
+	Datetime   = "datetime"
+)
+
 // Types maps model Type stirngs to go types. This should be extended as new
 // types are defined.
 var Types = map[string]gothicgo.Type{
-	"bool":       gothicgo.BoolType,
-	"byte":       gothicgo.ByteType,
-	"int":        gothicgo.IntType,
-	"int8":       gothicgo.Int8Type,
-	"int16":      gothicgo.Int16Type,
-	"int32":      gothicgo.Int32Type,
-	"int64":      gothicgo.Int64Type,
-	"complex128": gothicgo.Complex128Type,
-	"complex64":  gothicgo.Complex64Type,
-	"float32":    gothicgo.Float32Type,
-	"float64":    gothicgo.Float64Type,
-	"rune":       gothicgo.RuneType,
-	"string":     gothicgo.StringType,
-	"uint":       gothicgo.UintType,
-	"uint8":      gothicgo.Uint8Type,
-	"uint16":     gothicgo.Uint16Type,
-	"uint32":     gothicgo.Uint32Type,
-	"uint64":     gothicgo.Uint64Type,
-	"uintptr":    gothicgo.UintptrType,
-	"datetime":   gothicgo.DefStruct(gothicgo.MustPackageRef("time"), "Time"),
+	Bool:       gothicgo.BoolType,
+	Byte:       gothicgo.ByteType,
+	Int:        gothicgo.IntType,
+	Int8:       gothicgo.Int8Type,
+	Int16:      gothicgo.Int16Type,
+	Int32:      gothicgo.Int32Type,
+	Int64:      gothicgo.Int64Type,
+	Complex128: gothicgo.Complex128Type,
+	Complex64:  gothicgo.Complex64Type,
+	Float32:    gothicgo.Float32Type,
+	Float64:    gothicgo.Float64Type,
+	Rune:       gothicgo.RuneType,
+	String:     gothicgo.StringType,
+	Uint:       gothicgo.UintType,
+	Uint8:      gothicgo.Uint8Type,
+	Uint16:     gothicgo.Uint16Type,
+	Uint32:     gothicgo.Uint32Type,
+	Uint64:     gothicgo.Uint64Type,
+	Uintptr:    gothicgo.UintptrType,
+	Datetime:   gothicgo.DefStruct(gothicgo.MustPackageRef("time"), "Time"),
 }
 
 // Tags defines the tags that will set on a field
